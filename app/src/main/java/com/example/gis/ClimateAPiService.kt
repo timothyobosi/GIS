3c229e03613be9ad1fda9f97eff2e1b8e97750b4
timothyobosi <timothyobosi8@gmail.com>
2024-09-26 12:13:16 +0300

    initial commit

--- app/src/main/java/com/example/gis/ClimateAPiService.kt
+++ app/src/main/java/com/example/gis/ClimateAPiService.kt
@@ -1,4 +1,9 @@
 package com.example.gis
 
+import retrofit2.http.GET
+import retrofit2.http.Query
+
 interface ClimateAPiService {
+    @GET("climate/data")
+    suspend fun getClimateData(@Query("region")region: String): ClimateDataResponse
 }
---
+++ app/src/main/java/com/example/gis/ClimateDataResponse.kt
@@ -0,0 +1,7 @@
+package com.example.gis
+
+data class ClimateDataResponse(
+    val temperature: Double,
+    val rainfall : Double,
+    val humidity: Double
+)
